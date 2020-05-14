6b7b4313a9e355f4ec25d5511d70060dc92dfb52
seyoung-Oh <osy0602@naver.com>
2020-05-14 21:57:26 +0900

    Doublelinkedlist3.cpp 헤더파일

---
+++ List1.h
@@ -0,0 +1,68 @@
+#pragma once // 이 헤더 파일을 딱 1번만 포함시키겠다는 의미
+
+#include <iostream>
+
+using namespace std;
+
+enum OUTPUT_TYPE
+{
+	OT_NONE,
+	OT_OUTPUT,
+	OT_INVERSE
+};
+
+#define NAME_SIZE 32
+
+typedef struct _tagStudent
+{
+	char strName[NAME_SIZE];
+	int iNumber;
+	int iKor;
+	int iEng;
+	int iMath;
+	int iTotal;
+	int fAvg;
+}STUDENT, *PSTUDENT;
+
+typedef struct _tagNode
+{
+	STUDENT tStudent;
+	_tagNode* pNext;
+	_tagNode* pPrev;
+}NODE, *PNODE;
+
+typedef struct _tagList
+{
+	PNODE pBegin;
+	PNODE pEnd;
+	int iSize;
+}LIST, *PLIST;
+
+static int InputInt() {
+	int iInput;
+	cin >> iInput;
+	if (cin.fail()) {
+		cin.clear();
+		cin.ignore(1024, '\n');
+
+		return INT_MAX;
+	}
+	return iInput;
+}
+
+static void InputString(char* pString, int iSize)
+{
+	cin.clear();
+	cin.ignore(1024, '\n');
+	cin.getline(pString, iSize - 1);
+}
+
+void InitList(PLIST pList);
+//void CleartList(PLIST pList);
+void DestroyList(PLIST pList);
+void Push_Back(PLIST pList);
+void Push_Front(PLIST pList);
+void OutputStudent(const PSTUDENT pStudent);
+void Output(PLIST pList);
+void Search(PLIST pList);
+void Delete(PLIST pList);
