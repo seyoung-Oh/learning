9845c1f8d7761aacebeb52f1bdf4a9cc5fcf8f77
seyoung-Oh <osy0602@naver.com>
2020-06-07 10:44:10 +0900

    MapManager 소스파일

---
+++ MapManager.cpp
@@ -0,0 +1,49 @@
+#include "MapManager.h"
+#include "Stage.h"
+
+CMapManager* CMapManager::m_pInst = NULL;
+
+CMapManager::CMapManager()
+{
+	for (int i = 0; i < STAGE_MAX_COUNT; ++i)
+	{
+		m_pStage[i] = NULL;
+	}
+}
+
+CMapManager::~CMapManager()
+{
+	// 스테이지 메모리 해제
+	for (int i = 0; i < STAGE_MAX_COUNT; ++i)
+	{
+		SAFE_DELETE(m_pStage[i]);
+	}
+}
+
+CMapManager * CMapManager::GetInst()
+{
+	if (!m_pInst)
+		m_pInst = new CMapManager;
+
+	return m_pInst;
+}
+
+void CMapManager::DestroyInst()
+{
+	SAFE_DELETE(m_pInst);
+}
+
+bool CMapManager::Init()
+{
+	const char* pStageFile[3] = { "Stage1.txt","Stage2.txt","Stage3.txt" };
+
+	for (int i = 0; i < STAGE_MAX_COUNT; ++i)
+	{
+		m_pStage[i] = new CStage;
+
+		if (!m_pStage[i]->Init())
+			return false;
+	}
+
+	return true;
+}
