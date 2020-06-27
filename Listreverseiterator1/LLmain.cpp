3033732b5cf4c0904ba70348782a4249716953eb
seyoung-Oh <osy0602@naver.com>
2020-06-27 17:40:55 +0900

    리버스 이터레이터(템플릿 사용)

---
+++ Listreverseiterator1/LLmain.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "LinkedList.h"
+
+using namespace std;
+
+class CA
+{
+public:
+	CA()
+	{
+
+	}
+	~CA()
+	{
+
+	}
+public:
+	class CB
+	{
+	public:
+		CB()
+		{
+
+		}
+		~CB()
+		{
+
+		}
+	};
+};
+
+int main()
+{
+	CA a;
+	CA::CB b;
+
+	CLinkedList<int> listInt;
+	CLinkedList<float> listFloat;
+
+	for (int i = 0; i < 100;++i)
+	{
+		listInt.push_back(i + 1);
+	}
+
+	cout << listInt.size() << endl;
+
+	CLinkedList<int>::iterator iter;
+	//위와 아래는 같음
+	//CListIterator<int> iter;
+
+	for (iter = listInt.begin();iter != listInt.end();++iter)
+	{
+		cout << *iter << endl;
+	}
+
+ 	return 0;
+}
