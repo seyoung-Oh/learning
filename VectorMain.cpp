e8857fba66ceaf2f47657dae4f6d91c42cfe1dad
seyoung-Oh <osy0602@naver.com>
2020-06-30 23:14:56 +0900

    벡터 소스파일

---
+++ VectorMain.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Vector.h"
+
+using namespace std;
+
+int main()
+{
+	CVector<int> vecInt;
+
+	vecInt.reserve(100);
+
+	for (int i = 0; i < 100; ++i)
+	{
+		vecInt.push_back(i + 1);
+	}
+
+	for (int i = 0; i < vecInt.size();++i)
+	{
+		cout << vecInt[i] << endl;
+	}
+
+	return 0;
+}
