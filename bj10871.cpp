e73c814dc3b8f196349c417d7df93ecbc2b0a0a8
seyoung-Oh <osy0602@naver.com>
2020-08-30 23:17:51 +0900

    배열 너무 작게 설정해서 런타임 에러 났었음

---
+++ bj10871.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, x;
+
+	cin >> n >> x;
+
+	int arr[10000];
+
+	for (int i = 0;i < n;i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0;i < n;i++)
+	{
+		if (arr[i] < x)
+			cout << arr[i]  << " ";
+	}
+	cout << "\n";
+
+	return 0;
+}	
