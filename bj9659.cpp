44fd167c8bee7942090a14788357c78f032ecaa5
seyoung-Oh <osy0602@naver.com>
2020-12-07 23:16:17 +0900

    돌게임 5

---
+++ bj9659.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long n;
+	cin >> n;
+	if (n % 2 == 0)
+		cout << "CY" << endl;
+	else
+		cout << "SK" << endl;
+
+
+	return 0;
+
+}
